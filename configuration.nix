5f6c6d71685ce394b1dc47f41e12895063267b8d
getkirill <k.krasilnikov.2008@gmail.com>
2024-05-28 11:39:50 +0200

    frickin firewall was breaking my hotspot

--- configuration.nix
+++ configuration.nix
@@ -97,9 +97,9 @@
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedUDPPorts = [ 67 68 ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+#   networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
