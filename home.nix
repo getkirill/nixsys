c5cffed4f05de478025e5ba9bc691b17eb7e9fd4
getkirill <k.krasilnikov.2008@gmail.com>
2024-06-02 13:10:20 +0200

    katzeppuccin

--- home.nix
+++ home.nix
@@ -32,9 +32,35 @@
       theme = "avit";
     };
   };
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      colors.normal = {
+        black = "#45475a";
+        red = "#f38ba8";
+        green = "#a6e3a1";
+        yellow = "#f9e2af";
+        blue = "#89b4fa";
+        magenta = "#f5c2e7";
+        cyan = "#94e2d5";
+        white = "#bac2de";
+      };
+      colors.bright = {
+        black = "#45475a";
+        red = "#f38ba8";
+        green = "#a6e3a1";
+        yellow = "#f9e2af";
+        blue = "#89b4fa";
+        magenta = "#f5c2e7";
+        cyan = "#94e2d5";
+        white = "#bac2de";
+      };
+    };
+  };
   home.file.".local/share/applications/steam.desktop" = {
     source = ./steam-offload.desktop;
   };
+  
 
   home.stateVersion = "24.11"; # no touchy! >:(
   programs.home-manager.enable = true;
